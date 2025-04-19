5998449738664e93ea2c1c94e6ac6529dce7e6d2
moturri <moturri@users.noreply.github.com>
2025-04-19 18:44:14 +0300

    improvements on lsp

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,9 +1,9 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		version = "*",
 		lazy = false,
 		config = function()
+      require("fidget").setup({})
 			require("mason").setup()
 		end,
 	},
@@ -20,41 +20,58 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
-			local mason_lspconfig = require("mason-lspconfig")
 			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local default_config = {
-				capabilities = capabilities,
-				on_attach = function(_, bufnr)
-					local keymap = vim.keymap.set
-					local opts = { noremap = true, silent = true, buffer = bufnr }
-					keymap("n", "K", vim.lsp.buf.hover, opts)
-					keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
-					keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
-					keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-					keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					keymap("n", "<leader>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
+			-- Optional: enhance capabilities for folding, etc.
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
 			}
 
+			-- Attach function with navic + keymaps
+			local function on_attach(client, bufnr)
+				local keymap = vim.keymap.set
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+
+				-- LSP Keymaps
+				keymap("n", "K", vim.lsp.buf.hover, opts)
+				keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
+				keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
+				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap("n", "<leader>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+
+				-- Attach nvim-navic if available
+				local navic_ok, navic = pcall(require, "nvim-navic")
+				if navic_ok and client.server_capabilities.documentSymbolProvider then
+					navic.attach(client, bufnr)
+				end
+			end
+
+			-- Default LSP config
+			local default_config = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			}
+
+			-- Setup all servers, and customize as needed
 			mason_lspconfig.setup_handlers({
+				-- Default setup for all servers
 				function(server_name)
 					lspconfig[server_name].setup(default_config)
 				end,
+
+				-- Custom setup for Lua
 				["lua_ls"] = function()
 					lspconfig.lua_ls.setup(vim.tbl_deep_extend("force", default_config, {
 						settings = {
 							Lua = {
-								runtime = {
-									version = "LuaJIT",
-									path = vim.split(package.path, ";"),
-								},
-								diagnostics = {
-									globals = { "vim" },
-								},
+								runtime = { version = "LuaJIT" },
+								diagnostics = { globals = { "vim" } },
 								workspace = {
 									library = vim.api.nvim_get_runtime_file("", true),
 									checkThirdParty = false,
