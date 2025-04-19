3f34340ee9ce01e0b01553b6146a8db4a11bffa8
moturri <moturri@users.noreply.github.com>
2025-04-19 18:46:10 +0300

    minimized pop up

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,29 +1,29 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		version = "*",
-		config = function()
-			local oil = require("oil")
+    {
+        "stevearc/oil.nvim",
+        version = "*",
+        config = function()
+            local oil = require("oil")
 
-			oil.setup({
-				-- Optional configuration settings
-				columns = {
-					"icon",
-					"size",
-					"mtime",
-				},
-				view_options = {
-					show_hidden = true,
-				},
-				float = {
-					border = "rounded",
-					max_width = 0.9,
-					max_height = 0.9,
-				},
-			})
+            oil.setup({
+                -- Optional configuration settings
+                columns = {
+                    "icon",
+                    "size",
+                    "mtime",
+                },
+                view_options = {
+                    show_hidden = true,
+                },
+                float = {
+                    border = "rounded",
+                    max_width = 0.8,
+                    max_height = 0.8,
+                },
+            })
 
-			-- Key mapping to toggle the file explorer
-			vim.keymap.set("n", "-", oil.toggle_float, { desc = "Toggle Oil file explorer" })
-		end,
-	},
+            -- Key mapping to toggle the file explorer
+            vim.keymap.set("n", "-", oil.toggle_float, { desc = "Toggle Oil file explorer" })
+        end,
+    },
 }
