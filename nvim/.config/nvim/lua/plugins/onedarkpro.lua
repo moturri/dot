68f9154a55cf962ef46593c6b73868ada12fa12f
moturri <moturri@users.noreply.github.com>
2025-04-19 18:46:31 +0300

    minor changes

--- nvim/.config/nvim/lua/plugins/onedarkpro.lua
+++ nvim/.config/nvim/lua/plugins/onedarkpro.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"olimorris/onedarkpro.nvim",
-    version = "*",
-		priority = 1000, -- Ensure it loads first
-		config = function()
-			vim.cmd("colorscheme onedark_dark") -- Set the colorscheme
-		end,
-	},
+    {
+        "olimorris/onedarkpro.nvim",
+        version = "*",
+        priority = 1000, -- Ensure it loads first
+        config = function()
+            vim.cmd("colorscheme onedark_dark") -- Set the colorscheme
+        end,
+    },
 }
