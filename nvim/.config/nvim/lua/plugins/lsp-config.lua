83be5e533700c1944722c22b23427ed467140dcd
moturri <moturri@users.noreply.github.com>
2025-04-29 17:30:04 +0300

    code formatting

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,58 +1,58 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-      require("fidget").setup({})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+			require("fidget").setup({})
+		end,
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      automatic_installation = true,
-    },
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			automatic_installation = true,
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require("lspconfig")
-      local mason_lspconfig = require("mason-lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local mason_lspconfig = require("mason-lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local function on_attach(client, bufnr)
-        local opts = { noremap = true, silent = true, buffer = bufnr }
-        local keymap = vim.keymap.set
+			local function on_attach(client, bufnr)
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				local keymap = vim.keymap.set
 
-        keymap("n", "K", vim.lsp.buf.hover, opts)
-        keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
-        keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
-        keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        keymap("n", "<leader>f", function()
-          vim.lsp.buf.format({ async = true })
-        end, opts)
+				keymap("n", "K", vim.lsp.buf.hover, opts)
+				keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
+				keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
+				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				keymap("n", "<leader>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
 
-        local navic_ok, navic = pcall(require, "nvim-navic")
-        if navic_ok and client.server_capabilities.documentSymbolProvider then
-          navic.attach(client, bufnr)
-        end
-      end
+				local navic_ok, navic = pcall(require, "nvim-navic")
+				if navic_ok and client.server_capabilities.documentSymbolProvider then
+					navic.attach(client, bufnr)
+				end
+			end
 
-      local default_config = {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      }
+			local default_config = {
+				on_attach = on_attach,
+				capabilities = capabilities,
+			}
 
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          lspconfig[server_name].setup(default_config)
-        end,
-      })
-    end,
-  },
+			mason_lspconfig.setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup(default_config)
+				end,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,106 +1,106 @@
 return {
-    -- UI Select extension
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
+	-- UI Select extension
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
 
-    -- Main Telescope plugin
-    {
-        "nvim-telescope/telescope.nvim",
-        version = "*",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = function()
-            local actions = require("telescope.actions")
-            local themes = require("telescope.themes")
+	-- Main Telescope plugin
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = function()
+			local actions = require("telescope.actions")
+			local themes = require("telescope.themes")
 
-            return {
-                defaults = {
-                    prompt_prefix = "󰱼 ",
-                    selection_caret = "➜ ",
-                    entry_prefix = "  ",
-                    initial_mode = "insert",
-                    sorting_strategy = "ascending",
-                    layout_strategy = "flex",
-                    layout_config = {
-                        horizontal = { prompt_position = "top" },
-                        vertical = { prompt_position = "top" },
-                        flex = { flip_columns = 150 },
-                    },
-                    mappings = {
-                        i = {
-                            ["<C-n>"] = actions.move_selection_next,
-                            ["<C-p>"] = actions.move_selection_previous,
-                            ["<C-c>"] = actions.close,
-                            ["<C-u>"] = actions.preview_scrolling_up,
-                            ["<C-d>"] = actions.preview_scrolling_down,
-                        },
-                        n = {
-                            ["<C-c>"] = actions.close,
-                            ["<C-j>"] = actions.move_selection_next,
-                            ["<C-k>"] = actions.move_selection_previous,
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = themes.get_dropdown({}),
-                },
-            }
-        end,
-        config = function(_, opts)
-            local telescope = require("telescope")
-            telescope.setup(opts)
-            telescope.load_extension("ui-select")
-        end,
-        keys = {
-            {
-                "<leader>ff",
-                function()
-                    require("telescope.builtin").find_files()
-                end,
-                desc = "Find Files",
-            },
-            {
-                "<leader>fh",
-                function()
-                    require("telescope.builtin").help_tags()
-                end,
-                desc = "Help Tags",
-            },
-            {
-                "<leader>fc",
-                function()
-                    require("telescope.builtin").commands()
-                end,
-                desc = "Commands",
-            },
-            {
-                "<leader>fm",
-                function()
-                    require("telescope.builtin").man_pages()
-                end,
-                desc = "Man Pages",
-            },
-            {
-                "<leader>fr",
-                function()
-                    require("telescope.builtin").registers()
-                end,
-                desc = "Registers",
-            },
-            {
-                "<leader>fs",
-                function()
-                    require("telescope.builtin").search_history()
-                end,
-                desc = "Search History",
-            },
-            {
-                "<leader>f/",
-                function()
-                    require("telescope.builtin").current_buffer_fuzzy_find()
-                end,
-                desc = "Fuzzy Find in Current Buffer",
-            },
-        },
-    },
+			return {
+				defaults = {
+					prompt_prefix = "󰱼 ",
+					selection_caret = "➜ ",
+					entry_prefix = "  ",
+					initial_mode = "insert",
+					sorting_strategy = "ascending",
+					layout_strategy = "flex",
+					layout_config = {
+						horizontal = { prompt_position = "top" },
+						vertical = { prompt_position = "top" },
+						flex = { flip_columns = 150 },
+					},
+					mappings = {
+						i = {
+							["<C-n>"] = actions.move_selection_next,
+							["<C-p>"] = actions.move_selection_previous,
+							["<C-c>"] = actions.close,
+							["<C-u>"] = actions.preview_scrolling_up,
+							["<C-d>"] = actions.preview_scrolling_down,
+						},
+						n = {
+							["<C-c>"] = actions.close,
+							["<C-j>"] = actions.move_selection_next,
+							["<C-k>"] = actions.move_selection_previous,
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = themes.get_dropdown({}),
+				},
+			}
+		end,
+		config = function(_, opts)
+			local telescope = require("telescope")
+			telescope.setup(opts)
+			telescope.load_extension("ui-select")
+		end,
+		keys = {
+			{
+				"<leader>ff",
+				function()
+					require("telescope.builtin").find_files()
+				end,
+				desc = "Find Files",
+			},
+			{
+				"<leader>fh",
+				function()
+					require("telescope.builtin").help_tags()
+				end,
+				desc = "Help Tags",
+			},
+			{
+				"<leader>fc",
+				function()
+					require("telescope.builtin").commands()
+				end,
+				desc = "Commands",
+			},
+			{
+				"<leader>fm",
+				function()
+					require("telescope.builtin").man_pages()
+				end,
+				desc = "Man Pages",
+			},
+			{
+				"<leader>fr",
+				function()
+					require("telescope.builtin").registers()
+				end,
+				desc = "Registers",
+			},
+			{
+				"<leader>fs",
+				function()
+					require("telescope.builtin").search_history()
+				end,
+				desc = "Search History",
+			},
+			{
+				"<leader>f/",
+				function()
+					require("telescope.builtin").current_buffer_fuzzy_find()
+				end,
+				desc = "Fuzzy Find in Current Buffer",
+			},
+		},
+	},
 }
