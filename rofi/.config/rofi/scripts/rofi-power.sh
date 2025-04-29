c6d3ad424a7a60f4031dd1f6804882708c054a7e
moturri <moturri@users.noreply.github.com>
2025-04-29 16:08:13 +0300

    cleaned up power menu script

--- rofi/.config/rofi/scripts/rofi-power.sh
+++ rofi/.config/rofi/scripts/rofi-power.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 
-# Nerd Font icons with labels
 lock="󰌾  Lock"
 logout="  Logout"
 reboot="󱑞  Reboot"
 shutdown="󰐥  Shutdown"
 cancel="󰜺  Cancel"
 
-# Options array for the power menu
 options=(
-  "$lock"
-  "$logout"
-  "$reboot"
-  "$shutdown"
-  "$cancel"
+	"$lock"
+	"$logout"
+	"$reboot"
+	"$shutdown"
+	"$cancel"
 )
 
 choice=$(printf '%s\n' "${options[@]}" | rofi -dmenu -i -p "Power Menu")
 
 confirm_exit() {
-  echo -e "Yes\nNo" | rofi -dmenu -i -p "Are you sure?"
+	echo -e "Yes\nNo" | rofi -dmenu -i -p "Are you sure?"
 }
 
-# Handle user selection
 case "$choice" in
 "$lock")
-  i3lock -c 000000
-  ;;
+	i3lock -c 000000
+	;;
 "$logout")
-  qtile cmd-obj -o cmd -f shutdown
-  ;;
+	qtile cmd-obj -o cmd -f shutdown
+	;;
 "$reboot")
-  [[ "$(confirm_exit)" == "Yes" ]] && reboot
-  ;;
+	[[ "$(confirm_exit)" == "Yes" ]] && reboot
+	;;
 "$shutdown")
-  [[ "$(confirm_exit)" == "Yes" ]] && poweroff
-  ;;
+	[[ "$(confirm_exit)" == "Yes" ]] && poweroff
+	;;
 *)
-  exit 0
-  ;;
+	exit 0
+	;;
 esac
