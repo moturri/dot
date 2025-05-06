03a8fed1ea71846257170463dbf05c6bc041fffa
moturri <moturri@users.noreply.github.com>
2025-05-06 23:48:36 +0300

    auto mode for external monitor

--- qtile/.config/qtile/scripts/monitors.sh
+++ qtile/.config/qtile/scripts/monitors.sh
@@ -4,7 +4,7 @@ primaryMonitor="eDP-1"
 secondMonitor="DP-1"
 
 if xrandr | grep -q "${secondMonitor} connected"; then
-	xrandr --output "${secondMonitor}" --mode "1680x1050" --left-of "${primaryMonitor}"
+	xrandr --output "${secondMonitor}" --auto --left-of "${primaryMonitor}"
 else
 	xrandr --output "${secondMonitor}" --off
 fi
