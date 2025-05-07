d67dba8626ca2fe4f63b5d46fda9c1e28201b0f9
moturri <moturri@users.noreply.github.com>
2025-05-07 21:31:16 +0300

    refined the config file to work more efficiently. I need to read the documetaion more

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,126 +1,42 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		version = "*",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    version = "*", -- You could specify a stable version here
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "javascript",
+          "lua",
+          "typescript",
+          "python",           -- Limit this to what you need
+        },
+        sync_install = false, -- Already in place for faster startup
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false, -- Disabling regex highlighting
+          disable = { "markdown", "html" },          -- Disable for unnecessary languages
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = false, -- Disable if not using it
+        },
+        autotag = { enable = true },
+        textobjects = {
+          select = {
+            enable = false, -- Disable if not using it
+          },
+        },
+      })
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		version = "*",
-		lazy = false,
-		opts = {
-			automatic_installation = true,
-		},
-	},
-
-	{
-		"neovim/nvim-lspconfig",
-		version = "*",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local mason_lspconfig = require("mason-lspconfig")
-
-			-- Capabilities
-			local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-			local capabilities = has_cmp and cmp_nvim_lsp.default_capabilities()
-				or vim.lsp.protocol.make_client_capabilities()
-
-			-- LSP on_attach
-			local function on_attach(_, bufnr)
-				local keymap = vim.keymap.set
-				local opts = { noremap = true, silent = true, buffer = bufnr }
-
-				keymap("n", "K", vim.lsp.buf.hover, opts)
-				keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
-				keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
-				keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-				keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				keymap("n", "<leader>f", function()
-					vim.lsp.buf.format({ async = true })
-				end, opts)
-			end
-
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "",
-						[vim.diagnostic.severity.WARN] = "",
-						[vim.diagnostic.severity.HINT] = "",
-						[vim.diagnostic.severity.INFO] = "",
-					},
-				},
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			})
-
-			local default_config = {
-				on_attach = on_attach,
-				capabilities = capabilities,
-			}
-
-			mason_lspconfig.setup_handlers({
-				function(server_name)
-					lspconfig[server_name].setup(default_config)
-				end,
-			})
-		end,
-	},
-
-	{
-		"hrsh7th/nvim-cmp",
-		version = "*",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			version = "*",
-		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-			})
-		end,
-	},
-
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-		config = function() end,
-	},
+      -- Context commentstring module
+      vim.g.skip_ts_context_commentstring_module = true
+      require("ts_context_commentstring").setup({})
+    end,
+  },
 }
