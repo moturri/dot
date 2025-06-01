48d9d96ba79ec79d1efd2a19318a3a1b7163bfa9
moturri <moturri@users.noreply.github.com>
2025-06-02 00:20:51 +0300

    copyright

--- qtile/.config/qtile/wpctl.py
+++ qtile/.config/qtile/wpctl.py
@@ -1,3 +1,23 @@
+# Copyright (C) 2025, Elton Moturi
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import subprocess
 from typing import Any, List, Optional, Tuple
 
@@ -94,4 +114,3 @@ class AudioWidget(GenPollText):  # type: ignore
 class MicWidget(AudioWidget):
     def __init__(self, **config: Any) -> None:
         super().__init__(kind="input", device="@DEFAULT_AUDIO_SOURCE@", **config)
-
