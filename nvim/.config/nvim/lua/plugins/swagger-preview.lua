cdcc03c06347bdd54e4451d7da502f888a8af663
moturri <moturri@users.noreply.github.com>
2025-06-18 22:56:18 +0300

    removed swagger-preview.lua

--- nvim/.config/nvim/lua/plugins/swagger-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "vinnymeller/swagger-preview.nvim",
-  build = "npm install -g swagger-ui-watcher",
-  config = function()
-    require("swagger-preview").setup({})
-
-    vim.keymap.set("n", "<leader>sp", "<cmd>SwaggerPreview<CR>", { desc = "Open Swagger Preview" })
-  end,
-}
