6428ae1cb44aeba639893ce98d7bd13025360b0f
moturri <moturri@users.noreply.github.com>
2025-07-01 14:25:55 +0300

    compatibility with latest Neovim

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -30,23 +30,20 @@ return {
 				capabilities = cmp_lsp.default_capabilities(capabilities)
 			end
 
-			local signs = {
-				Error = "",
-				Warn = "",
-				Hint = "",
-				Info = "",
-			}
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
-
 			vim.diagnostic.config({
 				virtual_text = true,
 				underline = true,
 				severity_sort = true,
 				float = { border = "rounded" },
 				update_in_insert = false,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = "",
+						[vim.diagnostic.severity.WARN] = "",
+						[vim.diagnostic.severity.HINT] = "",
+						[vim.diagnostic.severity.INFO] = "",
+					},
+				},
 			})
 
 			local function on_attach(client, bufnr)
