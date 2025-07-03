14122eadce7b0f45482679189b325a64544b95f3
moturri <moturri@users.noreply.github.com>
2025-07-03 09:19:41 +0300

    refine qtile startup scripts

--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# picom &
-greenclip daemon &
-/usr/bin/lxqt-policykit-agent &
-dunst &
-nm-applet &
-# firewall-applet &
+function run_app() {
+    if ! pgrep -f "$1" > /dev/null; then
+        $@ &
+    fi
+}
+
+run_app greenclip daemon
+run_app /usr/bin/lxqt-policykit-agent
+run_app dunst
+run_app nm-applet
--- qtile/.config/qtile/scripts/monitors.sh
+++ qtile/.config/qtile/scripts/monitors.sh
@@ -4,7 +4,7 @@ primaryMonitor="eDP-1"
 secondMonitor="DP-1"
 
 if xrandr | grep -q "${secondMonitor} connected"; then
-	xrandr --output "${secondMonitor}" --auto --left-of "${primaryMonitor}"
+	xrandr --output "${secondMonitor}" --auto --left-of "${primaryMonitor}" || exit 1
 else
-	xrandr --output "${secondMonitor}" --off
+	xrandr --output "${secondMonitor}" --off || exit 1
 fi
