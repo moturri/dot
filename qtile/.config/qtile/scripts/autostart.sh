2281ba3e064525b6fc473f7a1c1c893b056add24
moturri <moturri@users.noreply.github.com>
2025-07-06 14:04:41 +0300

    autostart: kb option and system bell

--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+set -euo pipefail
+
 function run() {
   if ! pgrep -f "$1" >/dev/null; then
     "$@" &
   fi
 }
 
+# Disable system bell
+xset b off
+
+# Remap Caps Lock to Ctrl (X session only)
+setxkbmap -option ctrl:nocaps
+
+# Start apps
 run greenclip daemon
 run /usr/bin/lxqt-policykit-agent
 run dunst
 run nm-applet
-# run_app picom -b
+# run picom -b
