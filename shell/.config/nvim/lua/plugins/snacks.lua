b734fa7c5f5476354ddfead162cd7c3f0bc7fa16
moturri <moturri@users.noreply.github.com>
2025-07-08 10:25:10 +0300

    sacks: more config opts

--- shell/.config/nvim/lua/plugins/snacks.lua
+++ shell/.config/nvim/lua/plugins/snacks.lua
@@ -4,21 +4,80 @@ return {
 	lazy = false,
 	dependencies = {
 		"folke/which-key.nvim",
+		"echasnovski/mini.icons",
+		"nvim-tree/nvim-web-devicons",
 	},
 
 	opts = {
-		bigfile = { enabled = true },
-		dashboard = { enabled = true },
-		statuscolumn = { enabled = true },
+		bigfile = {
+			enabled = true,
+			notify = true,
+			size = 1.5 * 1024 * 1024,
+			line_length = 1000,
+			setup = function(ctx)
+				if vim.fn.exists(":NoMatchParen") ~= 0 then
+					vim.cmd([[NoMatchParen]])
+				end
+				Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+				vim.b.minianimate_disable = true
+				vim.schedule(function()
+					if vim.api.nvim_buf_is_valid(ctx.buf) then
+						vim.bo[ctx.buf].syntax = ctx.ft
+					end
+				end)
+			end,
+		},
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{
+					pane = 2,
+					section = "terminal",
+					cmd = "colorscript -e square",
+					height = 5,
+					padding = 1,
+				},
+				{ section = "keys", gap = 1, padding = 1 },
+				{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{
+					pane = 2,
+					icon = " ",
+					title = "Git Status",
+					section = "terminal",
+					enabled = function()
+						return Snacks.git.get_root() ~= nil
+					end,
+					cmd = "git status --short --branch --renames",
+					height = 5,
+					padding = 1,
+					ttl = 5 * 60,
+					indent = 3,
+				},
+				{ section = "startup" },
+			},
+		},
 		explorer = {
 			enabled = true,
-			replace_netrw = true,
-			hidden = true,
+			finder = "explorer",
+			sort = { fields = { "sort" } },
 			tree = true,
-			follow_file = true,
 			supports_live = true,
+			follow_file = true,
+			focus = "list",
+			auto_close = false,
+			jump = { close = false },
+			layout = { preset = "sidebar", preview = false },
+			formatters = { files = { filename_only = true } },
+			matcher = { sort_empty = true },
+			config = function(opts)
+				return require("snacks.picker.source.explorer").setup(opts)
+			end,
 		},
-		indent = { enabled = true },
+		indent = {
+			enabled = true,
+		},
 		input = { enabled = true },
 		notifier = {
 			enabled = true,
@@ -29,11 +88,16 @@ return {
 			tree = true,
 			hidden = true,
 		},
-		quickfile = { enabled = true },
-		scope = { enabled = true },
+		quickfile = {
+			enabled = true,
+			exclude = { "python" },
+		},
+		scope = {
+			enabled = true,
+		},
 		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
 		words = { enabled = true },
+		-- image = { enabled = true },
 		styles = {
 			notification = {
 				wo = { wrap = true },
