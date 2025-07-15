048efaf9b4ae75523fbf4a39519cd0f8aa6c9c25
moturri <moturri@users.noreply.github.com>
2025-07-15 12:51:11 +0300

    treesitter: simplify config and remove conflicting keymaps
    - Removed redundant languages and options
    - Trimmed non-functional or overlapping keymaps
    - Replaced <C-space> and <[i]> with stable alternatives
    - Improved reliability across terminals and Neovim defaults

--- shell/.config/nvim/lua/plugins/treesitter.lua
+++ shell/.config/nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,58 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "VeryLazy" },
-
+	lazy = false,
+	build = ":TSUpdate",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"windwp/nvim-ts-autotag",
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = vim.tbl_flatten({
-				{ "html", "css", "scss", "javascript", "typescript", "tsx", "vue", "svelte", "json" },
-				{ "bash", "lua", "vim", "vimdoc", "regex" },
-				{ "markdown", "markdown_inline", "yaml" },
-				{ "python" },
-				{ "latex", "typst" },
-				{ "norg" },
-			}),
-
-			sync_install = false,
+			ensure_installed = {
+				"html",
+				"css",
+				"scss",
+				"javascript",
+				"typescript",
+				"tsx",
+				"vue",
+				"svelte",
+				"json",
+				"bash",
+				"lua",
+				"vim",
+				"vimdoc",
+				"regex",
+				"markdown",
+				"markdown_inline",
+				"yaml",
+				"python",
+				"latex",
+				"typst",
+				"norg",
+			},
 			auto_install = true,
-
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = false,
-				disable = function(lang, bufnr)
-					local max_filesize = 100 * 1024
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
+				disable = function(_, bufnr)
+					local max = 100 * 1024
+					local ok, stat = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+					return ok and stat and stat.size > max
 				end,
 			},
-
-			indent = {
-				enable = true,
-				-- disable = { "yaml" },
-			},
+			indent = { enable = true },
 
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = "<C-s>",
-					node_decremental = "<C-backspace>",
+					init_selection = "gnn",
+					node_incremental = "grn",
+					node_decremental = "grm",
 				},
 			},
 
-			autotag = {
-				enable = true,
-			},
+			autotag = { enable = true },
 
 			textobjects = {
 				select = {
@@ -64,13 +65,6 @@ return {
 						["ic"] = "@class.inner",
 						["aa"] = "@parameter.outer",
 						["ia"] = "@parameter.inner",
-						["ai"] = "@conditional.outer",
-						["ii"] = "@conditional.inner",
-						["al"] = "@loop.outer",
-						["il"] = "@loop.inner",
-						["ab"] = "@block.outer",
-						["ib"] = "@block.inner",
-						["as"] = "@statement.outer",
 					},
 				},
 				move = {
@@ -80,29 +74,17 @@ return {
 						["]f"] = "@function.outer",
 						["]c"] = "@class.outer",
 						["]a"] = "@parameter.outer",
-						["]i"] = "@conditional.outer",
-						["]l"] = "@loop.outer",
-						["]b"] = "@block.outer",
-						["]s"] = "@statement.outer",
 					},
 					goto_previous_start = {
 						["[f"] = "@function.outer",
 						["[c"] = "@class.outer",
 						["[a"] = "@parameter.outer",
-						["[i]"] = "@conditional.outer",
-						["[l"] = "@loop.outer",
-						["[b"] = "@block.outer",
-						["[s"] = "@statement.outer",
 					},
 				},
 				swap = {
 					enable = true,
-					swap_next = {
-						["<leader>sn"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>sp"] = "@parameter.inner",
-					},
+					swap_next = { ["<leader>sn"] = "@parameter.inner" },
+					swap_previous = { ["<leader>sp"] = "@parameter.inner" },
 				},
 			},
 		})
